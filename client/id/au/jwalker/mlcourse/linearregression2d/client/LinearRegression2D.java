8dda525d91bd8a81784fb3cb013071af67310c55
James Walker <github@jzwalker.id.au>
2011-10-11 21:54:17 +1100

    Experiment with animating the appearance of areas when they are requested from the navigation menu

--- client/id/au/jwalker/mlcourse/linearregression2d/client/LinearRegression2D.java
+++ client/id/au/jwalker/mlcourse/linearregression2d/client/LinearRegression2D.java
@@ -1,21 +1,13 @@
 package id.au.jwalker.mlcourse.linearregression2d.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class LinearRegression2D
   implements EntryPoint
 {
-
-  /**
-   * This is the entry point method.
-   */
   public void onModuleLoad()
   {
-    //final DataSetCanvas ui = new DataSetCanvas();
     final ApplicationUI ui = new ApplicationUI();
 
     RootLayoutPanel.get().add( ui.asWidget() );
