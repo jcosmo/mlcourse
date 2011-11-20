d5d9b64d9d180b1f3fbad8da689f83dfe62673c5
James Walker <github@jzwalker.id.au>
2011-11-21 00:02:43 +1100

    Respond to click events on the canvas and plot the associated point

--- client/src/main/java/id/au/jwalker/mlcourse/client/lr2d/lr2dUI.java
+++ client/src/main/java/id/au/jwalker/mlcourse/client/lr2d/lr2dUI.java
@@ -5,6 +5,7 @@ import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.CssColor;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.touch.client.Point;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiFactory;
 import com.google.gwt.uibinder.client.UiField;
@@ -12,6 +13,7 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import java.util.ArrayList;
 
 public class lr2dUI
   extends Composite
@@ -21,69 +23,100 @@ public class lr2dUI
   {
   }
 
-  private static lr2dUIUiBinder ourUiBinder = GWT.create( lr2dUIUiBinder.class );
+  private final CssColor WHITE;
+  private final CssColor BLACK;
+  private final CssColor RED;
+  private final int WIDTH = 400;
+  private final int HEIGHT = 400;
+
+  private static lr2dUIUiBinder _ourUiBinder = GWT.create( lr2dUIUiBinder.class );
   @UiField
-  Button startButton;
+  Button _startButton;
   @UiField
-  Canvas canvas;
-
-  private Context2d backBufferContext;
-  private Context2d context;
-  private CssColor white;
-  private CssColor black;
-
-  static final int WIDTH = 400;
-  static final int HEIGHT = 400;
-
-  int posn = 1;
-
-  public lr2dUI()
-  {
-    initWidget( ourUiBinder.createAndBindUi( this ) );
-    final Canvas backBuffer = Canvas.createIfSupported();
-
-    canvas.setWidth( (WIDTH) + "px" );
-    canvas.setHeight( (HEIGHT) + "px" );
-    canvas.setCoordinateSpaceWidth( WIDTH );
-    canvas.setCoordinateSpaceHeight( HEIGHT );
-    backBuffer.setCoordinateSpaceWidth( WIDTH );
-    backBuffer.setCoordinateSpaceHeight( HEIGHT );
-
-    context = canvas.getContext2d();
-    backBufferContext = backBuffer.getContext2d();
-
-    white = CssColor.make("rgba(255,255,255,1)");
-    black = CssColor.make("rgba(255,0,0,1)");
-  }
-
-  @UiHandler( "startButton" )
-  void startCanvas( final ClickEvent e )
-  {
-    drawAxis();
-    context.drawImage( backBufferContext.getCanvas(), 0, 0 );
-  }
-
-  void drawAxis( )
-  {
-    backBufferContext.setFillStyle( white );
-    backBufferContext.fillRect( 0, 0, WIDTH, HEIGHT );
-    backBufferContext.setLineWidth( 1 );
-    backBufferContext.setStrokeStyle( black );
-    backBufferContext.beginPath();
-    backBufferContext.moveTo( WIDTH / 2, 0 );
-    backBufferContext.lineTo( WIDTH / 2, HEIGHT );
-    backBufferContext.closePath();
-    backBufferContext.stroke();
-    backBufferContext.beginPath();
-    backBufferContext.moveTo( 0, HEIGHT / 2 );
-    backBufferContext.lineTo( WIDTH, HEIGHT / 2 );
-    backBufferContext.closePath();
-    backBufferContext.stroke();
-  }
-
+  Canvas _canvas;
   @UiFactory
   Canvas constructCanvas()
   {
     return Canvas.createIfSupported();
+  }
+
+  private final Context2d _backBufferContext;
+  private final Context2d _context;
+  private final ArrayList<Point> _dataPoints;
+
+  public lr2dUI()
+  {
+    _dataPoints = new ArrayList<Point>();
+    initWidget( _ourUiBinder.createAndBindUi( this ) );
+    final Canvas backBuffer = Canvas.createIfSupported();
+
+    _canvas.setWidth( (WIDTH) + "px" );
+    _canvas.setHeight( (HEIGHT) + "px" );
+    _canvas.setCoordinateSpaceWidth( WIDTH );
+    _canvas.setCoordinateSpaceHeight( HEIGHT );
+    backBuffer.setCoordinateSpaceWidth( WIDTH );
+    backBuffer.setCoordinateSpaceHeight( HEIGHT );
+
+    _context = _canvas.getContext2d();
+    _backBufferContext = backBuffer.getContext2d();
+
+    WHITE = CssColor.make("rgba(255,255,255,1)");
+    BLACK = CssColor.make("rgba(0,0,0,1)");
+    RED = CssColor.make("rgb(255,0,0)");
+  }
+
+  @UiHandler( "_startButton" )
+  void startCanvas( final ClickEvent e )
+  {
+    _dataPoints.clear();
+    redraw();
+  }
+
+  private void redraw()
+  {
+    drawAxis();
+    drawPoints();
+    _context.drawImage( _backBufferContext.getCanvas(), 0, 0 );
+  }
+
+  void drawAxis( )
+  {
+    _backBufferContext.setFillStyle( WHITE );
+    _backBufferContext.fillRect( 0, 0, WIDTH, HEIGHT );
+    _backBufferContext.setLineWidth( 1 );
+    _backBufferContext.setStrokeStyle( BLACK );
+    _backBufferContext.beginPath();
+    _backBufferContext.moveTo( WIDTH / 2, 0 );
+    _backBufferContext.lineTo( WIDTH / 2, HEIGHT );
+    _backBufferContext.closePath();
+    _backBufferContext.stroke();
+    _backBufferContext.beginPath();
+    _backBufferContext.moveTo( 0, HEIGHT / 2 );
+    _backBufferContext.lineTo( WIDTH, HEIGHT / 2 );
+    _backBufferContext.closePath();
+    _backBufferContext.stroke();
+  }
+
+  private void drawPoints()
+  {
+    _backBufferContext.setStrokeStyle( RED );
+    for ( Point dataPoint : _dataPoints )
+    {
+      drawPoint( dataPoint );
+    }
+  }
+
+  private void drawPoint( final Point dataPoint )
+  {
+    _backBufferContext.strokeRect( dataPoint.getX(), dataPoint.getY(), 1, 1 );
+  }
+
+  @UiHandler( "_canvas" )
+  public void clicked( final ClickEvent clickEvent )
+  {
+    final Point point = new Point( clickEvent.getX(), clickEvent.getY() );
+    _dataPoints.add( point );
+    drawPoint( point );
+    _context.drawImage( _backBufferContext.getCanvas(), 0, 0 );
   }
 }
